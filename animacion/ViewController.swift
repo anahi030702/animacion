1663aa07c222acd361bdf091bdfab0408f576648
Alejandro Salazar <alejandrosalazarcom25@gmail.com>
2024-10-03 18:34:48 -0600

    3 de cotubre jueves

--- animacion/ViewController.swift
+++ animacion/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imvAnimacion: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let arreglo:[UIImage] = [UIImage(named: "camina1.png")!,
+            UIImage(named: "camina1.png")!,
+            UIImage(named: "camina2.png")!,
+            UIImage(named: "camina3.png")!,
+            UIImage(named: "camina4.png")!,
+            UIImage(named: "camina5.png")!,
+            UIImage(named: "camina6.png")!,
+            UIImage(named: "camina7.png")!,
+            UIImage(named: "camina8.png")!,
+            UIImage(named: "camina9.png")!
+        ]
+        
+        imvAnimacion.animationImages = arreglo
+        imvAnimacion.animationRepeatCount = 0
+        imvAnimacion.animationDuration = 1
     }
 
-
+    @IBAction func cambiarVelocidad(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func encenderApagar(_ sender: UISwitch) {
+        if sender.isOn{
+            imvAnimacion.startAnimating()
+        }else{
+            imvAnimacion.stopAnimating()
+        }
+    }
 }
 
